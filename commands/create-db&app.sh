6a26b46cd76a980a912db7399b6ca9ac32d3daa6
dpt1305 <tungplatin@gmail.com>
2024-11-26 12:16:15 +0700

    fix secret yaml

--- commands/create-db&app.sh
+++ commands/create-db&app.sh
@@ -4,10 +4,13 @@ cd ../deployment;
 # Kubec apply deployments and services
 kubectl apply -f pvc.yaml;
 kubectl apply -f pv.yaml;
+kubectl create -f  configmap.yaml
+kubectl create -f  secret.yaml
 kubectl apply -f postgresql-deployment.yaml;
 kubectl apply -f postgresql-service.yaml;
 kubectl apply -f  coworking.yaml
-kubectl create -f  configmap.yaml
+
+
 
 # Set up port-forwarding to `postgresql-service`
 kubectl port-forward service/postgresql-service 5433:5432 &;
@@ -20,11 +23,3 @@ PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d uda-db -p 5433 < ..
 
 # Close port-forwarding
 ps aux | grep 'kubectl port-forward' | grep -v grep | awk '{print $2}' | xargs -r kill
-
-# detele resources
-kubectl delete -f postgresql-deployment.yaml;
-kubectl delete -f postgresql-service.yaml;
-kubectl delete -f  coworking.yaml;
-kubectl delete -f  configmap.yaml
-kubectl delete -f pvc.yaml;
-kubectl delete -f pv.yaml;
---
+++ commands/delete-db&app.sh
@@ -0,0 +1,8 @@
+# detele resources
+kubectl delete -f postgresql-deployment.yaml;
+kubectl delete -f postgresql-service.yaml;
+kubectl delete -f  coworking.yaml;
+kubectl delete -f  configmap.yaml
+kubectl delete -f  secret.yaml
+kubectl delete -f pvc.yaml;
+kubectl delete -f pv.yaml;
