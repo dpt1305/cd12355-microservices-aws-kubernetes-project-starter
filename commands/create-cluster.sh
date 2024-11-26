c82e2016695b4745e1ae7d75e1b28a77c270c61f
dpt1305 <tungplatin@gmail.com>
2024-11-26 21:25:23 +0700

    IoC add-on

--- commands/create-cluster.sh
+++ commands/create-cluster.sh
@@ -8,5 +8,17 @@ eksctl create cluster --name uda-cluster \
     --node-type t3.small \
     --nodes 1 --nodes-min 1 --nodes-max 2;
 
+# add Add-on
+# aws iam attach-role-policy \
+# --role-name my-worker-node-role \
+# --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy;
+aws eks create-addon \
+    --addon-name amazon-cloudwatch-observability \
+    --cluster-name uda-cluster \
+    --region us-east-1;
+
 # update config to local kube
 aws eks --region us-east-1 update-kubeconfig --name uda-cluster;
+
+# view all config of kubectl
+kubectl config view
